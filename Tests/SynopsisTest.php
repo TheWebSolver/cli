da2b915ff5749486301d0ed22bbe713ae1c9e806
Shesh Ghimire <sheshgh@outlook.com>
2025-01-08 18:12:32 +0545

    Fixed Test file namespace.

--- Tests/SynopsisTest.php
+++ Tests/SynopsisTest.php
@@ -1,7 +1,7 @@
 <?php
 declare( strict_types = 1 );
 
-namespace TheWebSolver\Codegarage\Cli;
+namespace TheWebSolver\Codegarage;
 
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
