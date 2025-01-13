6969a6ef304481b837951d23bfb068435bd98bc3
Shesh Ghimire <sheshgh@outlook.com>
2025-01-13 07:04:32 +0545

    Attribute parsing helper methods.

--- Src/Helper/Parser.php
+++ Src/Helper/Parser.php
@@ -1,10 +1,17 @@
-<?php
+<?php // phpcs:disable Squiz.Commenting.FunctionComment.IncorrectTypeHint
 declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Cli\Helper;
 
 use Closure;
 use BackedEnum;
+use ReflectionClass;
+use ReflectionAttribute;
+use TheWebSolver\Codegarage\Cli\Data\Flag;
+use Symfony\Component\Console\Input\InputOption;
+use TheWebSolver\Codegarage\Cli\Data\Positional;
+use TheWebSolver\Codegarage\Cli\Data\Associative;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Completion\Suggestion;
 use Symfony\Component\Console\Completion\CompletionInput;
 use Symfony\Component\Console\Completion\CompletionSuggestions;
@@ -26,5 +33,47 @@ class Parser {
 			is_array( $value )    => $value,
 			default               => (array) self::parseBackedEnumValue( $value )
 		};
+	}
+
+	/**
+	 * @param class-string<TAttribute>                       $attributeName
+	 * @param class-string<TTarget>|ReflectionClass<TTarget> $target
+	 * @return ?array<ReflectionAttribute<TAttribute>>
+	 * @template TAttribute of object
+	 * @template TTarget of object
+	 */
+	public static function parseClassAttribute( // phpcs:ignore Squiz.Commenting.FunctionComment.IncorrectTypeHint
+		string $attributeName,
+		string|ReflectionClass $target
+	): ?array {
+		$reflection = $target instanceof ReflectionClass ? $target : new ReflectionClass( $target );
+
+		return empty( $attrs = $reflection->getAttributes( $attributeName ) ) ? null : $attrs;
+	}
+
+	/**
+	 * @param class-string<TAttribute>                       $attributeName
+	 * @param class-string<TTarget>|ReflectionClass<TTarget> $target
+	 * @return (
+	 *   $toInput is true
+	 *     ? ($attributeName is class-string<Positional> ? ?InputArgument[] : ?InputOption[])
+	 *     : ?TAttribute[]
+	 * )
+	 * @template TAttribute of Positional|Associative|Flag
+	 * @template TTarget of object
+	 */
+	public static function parseInputAttribute(
+		string $attributeName,
+		string|ReflectionClass $target,
+		bool $toInput
+	): ?array {
+		if ( ! $attributes = self::parseClassAttribute( $attributeName, $target ) ) {
+			return null;
+		}
+
+		return array_map(
+			static fn( ReflectionAttribute $a ) => $toInput ? $a->newInstance()->input() : $a->newInstance(),
+			$attributes
+		);
 	}
 }
