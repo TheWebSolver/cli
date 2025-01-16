f26571150b6b303b9cdbb800b76249f1b405e6f0
Shesh Ghimire <sheshgh@outlook.com>
2025-01-16 12:09:32 +0545

    `InputExtractor` refactoring & enhancement.

--- Src/Helper/InputExtractor.php
+++ Src/Helper/InputExtractor.php
@@ -4,7 +4,6 @@ declare( strict_types = 1 );
 namespace TheWebSolver\Codegarage\Cli\Helper;
 
 use ReflectionClass;
-use ReflectionAttribute;
 use TheWebSolver\Codegarage\Cli\Console;
 use TheWebSolver\Codegarage\Cli\Data\Flag;
 use TheWebSolver\Codegarage\Cli\Data\Positional as Pos;
@@ -14,27 +13,36 @@ class InputExtractor {
 	final public const EXTRACT_AND_REPLACE = 1;
 	final public const EXTRACT_AND_UPDATE  = 2;
 
+	final public const IMMUTABLE_INPUT_PROPERTIES = array( 'name', 'mode' );
+
 	/** @var self::EXTRACT_AND* */
 	private int $flag = self::EXTRACT_AND_REPLACE;
 
 	/** @var array{0:Pos|Assoc|Flag,1:int|string} */
-	private array $inputAndProp;
+	private array $inputAndProperty;
 
 	/** @var array<class-string<Pos|Assoc|Flag>,array<string,Pos|Assoc|Flag>> */
 	private array $collect;
 
-	/** @var array<class-string<Console>,array<class-string<Pos|Assoc|Flag>,array<string,string[]>>> */
+	/** @var array<class-string<Console>,array<class-string<Pos|Assoc|Flag>,array<string,array<int|string>>>> */
 	private array $source;
 
 	/** @var ReflectionClass<Console> */
 	private ReflectionClass $target;
 
+	/** @var class-string<Console> */
+	private string $currentConsoleClass;
+
+	/** @var mixed[] */
+	private array $currentArguments;
+
 	/** @var array<class-string<Pos|Assoc|Flag>,array<string,array<array-key,mixed>>> */
-	private array $updates;
+	private array $update;
 
 	/** @param class-string<Console>|ReflectionClass<Console> $target */
 	public function __construct( string|ReflectionClass $target ) {
-		$this->target = $target instanceof ReflectionClass ? $target : new ReflectionClass( $target );
+		$this->target              = $target instanceof ReflectionClass ? $target : new ReflectionClass( $target );
+		$this->currentConsoleClass = $this->target->name;
 	}
 
 	/** @return array<string,array<string,Pos|Assoc|Flag>> */
@@ -42,7 +50,7 @@ class InputExtractor {
 		return $this->collect;
 	}
 
-	/** @return array<class-string<Console>,array<class-string<Pos|Assoc|Flag>,array<string,string[]>>> */
+	/** @return array<class-string<Console>,array<class-string<Pos|Assoc|Flag>,array<string,array<int|string>>>> */
 	public function getUpdateSource(): array {
 		return $this->source;
 	}
@@ -60,29 +68,29 @@ class InputExtractor {
 	}
 
 	public function extractAssociative(): self {
-		$this->extractUsing( $this->target, Assoc::class );
+		$this->extractFrom( $this->target, Assoc::class );
 
-		if ( ! $parent = $this->target->getParentClass() ) {
+		if ( ! $parent = $this->targetParentClass() ) {
 			return $this;
 		}
 
 		while ( $parent ) {
-			if ( Console::class === $parent->getName() ) {
-				break;
-			}
+			$this->currentConsoleClass = $parent->getName();
 
-			if ( ! $this->shouldUpdate() ) {
-				$this->extractUsing( $parent, Assoc::class );
+			if ( $this->shouldUpdate() ) {
+				$this->extractAndUpdateFrom( $parent, Assoc::class );
 			} else {
-				$this->extractAndAssignUsing( $parent, Assoc::class );
+				$this->extractFrom( $parent, Assoc::class );
 			}
 
 			$parent = $parent->getParentClass();
 		}
 
 		if ( $this->shouldUpdate() ) {
-			$this->applyUpdates();
+			$this->updateCollectionOf( Assoc::class );
 		}
+
+		$this->reset();
 
 		return $this;
 	}
@@ -91,18 +99,23 @@ class InputExtractor {
 	 * @param ReflectionClass<Console>     $reflection
 	 * @param class-string<Pos|Assoc|Flag> $attributeName
 	 */
-	private function extractUsing( ReflectionClass $reflection, string $attributeName ): void {
+	private function extractFrom( ReflectionClass $reflection, string $attributeName ): void {
 		foreach ( $reflection->getAttributes( $attributeName ) as $attribute ) {
-			$input = $attribute->newInstance();
+			$input                  = $attribute->newInstance();
+			$this->inputAndProperty = array( $input, '' );
+			$this->currentArguments = $attribute->getArguments();
 
-			if ( $this->inputAlreadyCollectedUsing( $attributeName, $input->name ) ) {
+			if ( $this->isCurrentInputInCollectionQueue() ) {
 				continue;
 			}
 
-			$this->collectInputUsing( $reflection->getName(), $args = $attribute->getArguments(), $input );
+			$this->pushCurrentInputToCollectionQueue();
 
 			if ( $this->shouldUpdate() ) {
-				$this->updates[ $input::class ][ $input->name ] = $args;
+				$this->update[ $input::class ][ $input->name ] = array(
+					'name' => $input->name,
+					...$this->onlyNamedArguments(),
+				);
 			}
 		}
 	}
@@ -111,99 +124,123 @@ class InputExtractor {
 	 * @param ReflectionClass<Console>     $reflection
 	 * @param class-string<Pos|Assoc|Flag> $attributeName
 	 */
-	private function extractAndAssignUsing( ReflectionClass $reflection, string $attributeName ): void {
+	private function extractAndUpdateFrom( ReflectionClass $reflection, string $attributeName ): void {
 		foreach ( $reflection->getAttributes( $attributeName ) as $attribute ) {
-			$input = $attribute->newInstance();
+			$input                  = $attribute->newInstance();
+			$this->currentArguments = $attribute->getArguments();
+			$this->inputAndProperty = array( $input, '' );
 
-			if ( ! $this->inputAlreadyCollectedUsing( $attributeName, $input->name ) ) {
-				$this->collectInputUsing( $reflection->getName(), $attribute->getArguments(), $input );
+			if ( ! $this->isCurrentInputInCollectionQueue() ) {
+				$this->pushCurrentInputToCollectionQueue();
 
 				continue;
 			}
 
-			$this->updateInputAlreadyCollectedUsingCurrentClass( $attribute, $reflection->getName(), $input );
+			$this->updateCollectionFromCurrentInput();
 		}
 	}
 
-	/**
-	 * @param class-string<Console> $reflectionClass
-	 * @param mixed[]               $args
-	 */
-	private function collectInputUsing( string $reflectionClass, array $args, Pos|Assoc|Flag $input ): void {
-		$sourced = array_filter( array_keys( $args ), $this->isCollectable( ... ) );
+	/** @param class-string<Pos|Assoc|Flag> $attributeName */
+	private function updateCollectionOf( string $attributeName ): void {
+		if ( ! $updatesInQueue = ( $this->update[ $attributeName ] ?? false ) ) {
+			return;
+		}
 
-		$this->collect[ $input::class ][ $input->name ]                    = $input;
-		$this->source[ $reflectionClass ][ $input::class ][ $input->name ] = $sourced;
-	}
-
-	private function applyUpdates(): void {
-		foreach ( $this->updates as $attributeName => $inputs ) {
-			foreach ( $inputs as $inputName => $updates ) {
-				if ( $input = $this->inputAlreadyCollectedUsing( $attributeName, $inputName ) ) {
-					$this->collect[ $attributeName ][ $inputName ] = $input->with( $updates );
-				}
+		foreach ( $updatesInQueue as $inputName => $updates ) {
+			if ( $input = $this->isCurrentInputInCollectionQueue( $inputName ) ) {
+				$this->collect[ $attributeName ][ $inputName ] = $input->with( $updates );
 			}
 		}
 	}
 
-	/**
-	 * @param ReflectionAttribute<Pos|Assoc|Flag> $attribute
-	 * @param class-string<Console>               $reflectionClass
-	 * @param Pos|Assoc|Flag                      $input
-	 */
-	private function updateInputAlreadyCollectedUsingCurrentClass(
-		ReflectionAttribute $attribute,
-		string $reflectionClass,
-		Pos|Assoc|Flag $input
-	): void {
-		foreach ( get_object_vars( $input ) as $property => $value ) {
-			$this->inputAndProp = array( $input, $property );
+	private function pushCurrentInputToCollectionQueue(): void {
+		$args = $this->currentArguments;
 
-			if ( $this->propertyAlreadySet() ) {
+		if ( $this->shouldUpdate() ) {
+			$args = $this->onlyNamedArguments();
+		}
+
+		[$input]                                        = $this->inputAndProperty;
+		$this->collect[ $input::class ][ $input->name ] = $input;
+
+		$this->source[ $this->currentConsoleClass ][ $input::class ][ $input->name ] = array_keys( $args );
+	}
+
+	private function updateCollectionFromCurrentInput(): void {
+		foreach ( get_object_vars( $this->inputAndProperty[0] ) as $property => $value ) {
+			$this->inputAndProperty[1] = $property;
+
+			if ( $this->isCurrentPropertyInUpdateQueue() ) {
 				continue;
 			}
 
-			if ( ! $this->propertiesAssignedInCurrentClass( $attribute ) ) {
+			if ( ! $this->currentPropertyIsNamedArgument() ) {
 				continue;
 			}
 
-			$defaultPropValue = $this->defaultPropValueAssigned();
-			$this->updates[ $input::class ][ $input->name ][ $property ] = $defaultPropValue ?? $value;
-
-			if ( $this->isCollectable( $property ) ) {
-				$this->source[ $reflectionClass ][ $input::class ][ $input->name ][] = $property;
-			}
+			$this->pushCurrentPropertyToUpdateQueue( $value );
 		}
+	}
+
+	private function pushCurrentPropertyToUpdateQueue( mixed $value ): void {
+		[$input, $property] = $this->inputAndProperty;
+		$defaultPropValue   = $this->defaultPropertyValueAssigned();
+
+		$this->update[ $input::class ][ $input->name ][ $property ] = $defaultPropValue ?? $value;
+
+		$this->source[ $this->currentConsoleClass ][ $input::class ][ $input->name ][] = $property;
+	}
+
+	private function reset(): void {
+		$this->currentArguments    = array();
+		$this->inputAndProperty    = array();
+		$this->update              = array();
+		$this->currentConsoleClass = $this->target->name;
+	}
+
+	private function isCurrentInputInCollectionQueue(): Pos|Assoc|Flag|null {
+		[$input]       = $this->inputAndProperty;
+		$attributeName = $input::class;
+		$inputName     = func_num_args() !== 1 ? $input->name : func_get_arg( 0 );
+
+		return $this->collect[ $attributeName ][ $inputName ] ?? null;
+	}
+
+	private function isCurrentPropertyInUpdateQueue(): bool {
+		[$input, $property] = $this->inputAndProperty;
+
+		return isset( $this->update[ $input::class ][ $input->name ][ $property ] );
+	}
+
+	private function currentPropertyIsNamedArgument(): bool {
+		[, $property] = $this->inputAndProperty;
+
+		return $this->isCollectable( $property ) && array_key_exists( $property, $this->currentArguments );
 	}
 
 	private function shouldUpdate(): bool {
 		return self::EXTRACT_AND_UPDATE === $this->flag;
 	}
 
-	/** @param class-string<Pos|Assoc|Flag> $attributeName */
-	private function inputAlreadyCollectedUsing( string $attributeName, string $inputName ): Pos|Assoc|Flag|null {
-		return $this->collect[ $attributeName ][ $inputName ] ?? null;
-	}
-
+	/** @phpstan-assert-if-true =string $property */
 	private function isCollectable( string|int $property ): bool {
-		return ! is_int( $property ) && 'name' !== $property;
+		return ! is_int( $property ) && ! in_array( $property, self::IMMUTABLE_INPUT_PROPERTIES, true );
 	}
 
-	private function propertyAlreadySet(): bool {
-		[$input, $prop] = $this->inputAndProp;
-
-		return 'mode' !== $prop && isset( $this->updates[ $input::class ][ $input->name ][ $prop ] );
+	/** @return ReflectionClass<Console> */
+	private function targetParentClass(): ?ReflectionClass {
+		return ( $p = $this->target->getParentClass() ) && Console::class !== $p->getName() ? $p : null;
 	}
 
-	/** @param ReflectionAttribute<Pos|Assoc|Flag> $attribute */
-	private function propertiesAssignedInCurrentClass( ReflectionAttribute $attribute ): bool {
-		return array_key_exists( $this->inputAndProp[1], $attribute->getArguments() );
+	/** @return array<string,mixed> */
+	private function onlyNamedArguments(): array {
+		return array_filter( $this->currentArguments, $this->isCollectable( ... ), mode: ARRAY_FILTER_USE_KEY );
 	}
 
-	private function defaultPropValueAssigned(): mixed {
-		[$input, $prop] = $this->inputAndProp;
+	private function defaultPropertyValueAssigned(): mixed {
+		[$input, $property] = $this->inputAndProperty;
 
-		return 'default' === $prop && method_exists( $input, 'getUserDefault' )
+		return 'default' === $property && method_exists( $input, 'getUserDefault' )
 			? $input->getUserDefault()
 			: null;
 	}
