2a1672efa9aec653e416748b985a3c4ea3ea392d
Shesh Ghimire <sheshgh@outlook.com>
2025-01-28 13:17:25 +0545

    Removed unnecessary InputAttribute helpers.

--- Src/Enums/InputVariant.php
+++ Src/Enums/InputVariant.php
@@ -3,11 +3,9 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Cli\Enums;
 
-use TheWebSolver\Codegarage\Cli\Console;
 use TheWebSolver\Codegarage\Cli\Data\Flag;
 use TheWebSolver\Codegarage\Cli\Data\Positional;
 use TheWebSolver\Codegarage\Cli\Data\Associative;
-use TheWebSolver\Codegarage\Cli\Helper\InputAttribute;
 
 enum InputVariant: string {
 	case Positional  = 'argument';
@@ -23,25 +21,8 @@ enum InputVariant: string {
 		};
 	}
 
-	/** @param class-string<Console> $className */
-	public function extractFrom( string $className, bool $overrideParent = false ): InputAttribute {
-		return InputAttribute::from( $className )
-			->do( self::perform( $overrideParent ), $this );
-	}
-
-	/** @param class-string<Console> $className */
-	public static function extractAllFrom( string $className, bool $overrideParent = false ): InputAttribute {
-		return InputAttribute::from( $className )
-			->do( self::perform( $overrideParent ) );
-	}
-
 	/** @param class-string<Positional|Associative|Flag> $className */
 	public static function fromAttribute( string $className ): ?self {
 		return array_filter( self::cases(), static fn( self $v ) => $v->getClassName() === $className )[0] ?? null;
-	}
-
-	/** @return InputAttribute::INFER_AND_* */
-	private static function perform( bool $override ): int {
-		return $override ? InputAttribute::INFER_AND_REPLACE : InputAttribute::INFER_AND_UPDATE;
 	}
 }
