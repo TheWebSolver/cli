690ddef93f5f46bc01cb04c455421a43396bed99
Shesh Ghimire <sheshgh@outlook.com>
2025-09-24 10:27:53 +0545

    update test: remove dependency on other packages for `CompilableCommandLoaderTest`.

--- Tests/Adapter/CompilableCommandLoaderTest.php
+++ Tests/Adapter/CompilableCommandLoaderTest.php
@@ -3,45 +3,31 @@ declare( strict_types = 1 );
 
 namespace TheWebSolver\Codegarage\Test\Adapter;
 
-use Composer\InstalledVersions;
 use PHPUnit\Framework\TestCase;
 use TheWebSolver\Codegarage\Cli\Cli;
 use Psr\Container\ContainerInterface;
 use PHPUnit\Framework\Attributes\Test;
-use TheWebSolver\Codegarage\Iso\Cli\Iso3166Command;
-use TheWebSolver\Codegarage\Iso\Cli\Iso4217Command;
+use TheWebSolver\Codegarage\Test\Fixture\TableConsoleCommand;
 use TheWebSolver\Codegarage\Cli\Adapter\CompilableCommandLoader;
 
 class CompilableCommandLoaderTest extends TestCase {
 	#[Test]
 	public function createsCompilableCommandsForIsoPackage(): void {
-		$isoCli = InstalledVersions::getInstallPath( 'thewebsolver/iso-cli' );
-		$loader = CompilableCommandLoader::start()
-			->inDirectory( "{$isoCli}/Src", 'TheWebSolver\Codegarage\Iso\Cli' )
-			->load( $this->getContainerMock() );
+		$fixtureDir    = dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'Fixture';
+		$compiledArray = CompilableCommandLoader::start()
+			->inDirectory( $fixtureDir, 'TheWebSolver\Codegarage\Test\Fixture' )
+			->load( $this->getContainerMock() )
+			->getArrayFile()->getContent();
 
-		$this->assertArrayHasKey( Iso3166Command::class, $loader->getArrayFile()->getContent() );
-		$this->assertArrayHasKey( Iso4217Command::class, $loader->getArrayFile()->getContent() );
+		$this->assertSame( [ TableConsoleCommand::class, 'start' ], $compiledArray[ TableConsoleCommand::class ] );
 	}
 
 	private function getContainerMock(): ContainerInterface {
-		( $container = $this->createMock( Container::class ) )
+		( $container = $this->createMock( ContainerInterface::class ) )
 			->method( 'get' )
 			->with( Cli::class )
 			->willReturn( $this->createStub( Cli::class ) );
 
 		return $container;
-	}
-}
-
-// phpcs:disable Generic.Files.OneObjectStructurePerFile.MultipleFound
-
-class Container implements ContainerInterface {
-	public function set( string $id ): void {}
-	public function get( string $id ): mixed {
-		return null;
-	}
-	public function has( string $id ): bool {
-		return false;
 	}
 }
